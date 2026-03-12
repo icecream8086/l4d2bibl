2298e70e87f43c9193e16396925a9967d16b172e
LinLai <zzbsn.cn@outlook.com>
2026-03-12 10:23:42 +0800

    fix: remove .gitignore and temporary files from git tracking

--- docs/.vuepress/.temp/git/config.js
+++
@@ -1,9 +0,0 @@
-import { GitContributors } from "C:/Users/zzbsn/source/repos/VsCode/alibabacloud/l4d2bibl/vuepress-starter/node_modules/@vuepress/plugin-git/lib/client/components/GitContributors.js";
-import { GitChangelog } from "C:/Users/zzbsn/source/repos/VsCode/alibabacloud/l4d2bibl/vuepress-starter/node_modules/@vuepress/plugin-git/lib/client/components/GitChangelog.js";
-
-export default {
-  enhance: ({ app }) => {
-    app.component("GitContributors", GitContributors);
-    app.component("GitChangelog", GitChangelog);
-  },
-};
